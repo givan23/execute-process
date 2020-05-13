615f51b26a306621d46c40a56de9fc0fe7891119
ivan.trabucco <ivan.trabucco@proximanetwork.it>
2020-05-13 13:21:23 +0200

    reorganized less folder tree

--- app/Clientlibs/Clientlib-all.js
+++ app/Clientlibs/Clientlib-all.js
@@ -1,5 +1,23 @@
-import '../../public/styles/Clientlib-all.less';
-import '../ExternalModules/devtool-module/style/index-devtool.less';
+///////////////////////////////////////
+//
+// CLIENTLIB COMMON - (external. this import is temporary to compile the less files)
+//
+///////////////////////////////////////
+import '../ExternalModules/common-module/styles/clientlib-common.less';
 
 
+///////////////////////////////////////
+//
+// CLIENTLIB DEVTOOL - (external. this import is temporary to compile the less files)
+//
+///////////////////////////////////////
+import '../ExternalModules/devtool-module/style/clientlib-devtool.less';
+
+
+///////////////////////////////////////
+//
+// CLIENTLIB INTERNAL
+//
+///////////////////////////////////////
+import '../../public/styles/clientlib-internal.less';
 
